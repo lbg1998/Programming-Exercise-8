84702653f1f37af7c0da9b40c9c7db5c8c4d20b5
lbg1998 <33192218+lbg1998@users.noreply.github.com>
2017-12-04 12:09:44 -0600

    Add files via upload

---
+++ Exercise14_4.java
@@ -0,0 +1,40 @@
+/**
+ * Created by Byung Gil Lee
+ * It is my own work.
+ */
+
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;public class Exercise14_4 extends Application { 
+
+    public void start(Stage primaryStage) {
+        GridPane pane = new GridPane();
+        pane.setAlignment(Pos.CENTER);
+        Label[] labels = new Label[5];
+        for (int i = 0; i < labels.length; i++) {
+            labels[i] = new Label("Java");
+            labels[i].setFont(Font.font("Times New Roman", FontWeight.BOLD, FontPosture.ITALIC, 22));
+            labels[i].setTextFill(getRandomColor());
+            labels[i].setRotate(90);
+            pane.add(labels[i], i, 0);
+        }
+        Scene scene = new Scene(pane, 250, 100);
+        primaryStage.setTitle("Java text");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    private Color getRandomColor() {
+        return new Color(Math.random(), Math.random(), Math.random(), Math.random());
+    }
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
